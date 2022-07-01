7aed9aa10fa1d8c98708784b9c569e3bd901d806
Yenaro Joel Noa Camino <97652778+Diegoo11@users.noreply.github.com>
2022-07-01 02:54:54 +0000

    test coding

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+import {DateTime} from 'luxon'
 console.log('Hello world')
+console.log('pato')
+
+const obj = {
+  name: "Pato",
+  action: "Camina",
+  alimento : "granos"
+}
+
+function consola(msj) {
+  console.table(msj)
+}
+
+consola("Ya dile a tus papas que te vas con un loco que no te para de amar")
+
